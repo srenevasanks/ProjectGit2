4616db83f13433279e4c63f99629d92a619e1792
Srenevasan <Srenevasan@Srenevasan-PC>
2020-01-18 12:03:58 +0530

    first all push

---
+++ ProjectGit2/src/main/java/sample/Google.java
@@ -0,0 +1,16 @@
+package sample;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Google {
+	@Test
+	public  void google() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		Thread.sleep(2000);
+		driver.close();
+	}
+}
